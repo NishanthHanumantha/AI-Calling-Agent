66ee87acf162268447d0d062b2fd9d9b53c9598f
Nishanth Bilimagga Hanumantha <nishanthbilimagga7@gmail.com>
2026-05-25 13:11:52 +0530

    Add version 2 and version 3 files

--- make_call.py
+++ make_call.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = "SID"
+account_sid = "AC8aa1a9255159e015d82a4b753cd76ad6"
 
-auth_token = "TOKEN"
+auth_token = "68ed8b421f4a4ce7ca8c436d16a168ba"
 
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
-    to="CUSTOMER NUMBER",
-    from_="DIALER NUMBER",
+    to="+918095841985",
+    from_="+15673722697",
     url="https://candied-attest-varying.ngrok-free.dev/voice"
 )
 
